24ebc984771934cb9dd9a64e0828ce1f52262a38
temilitt <aderounmutemiloluwa2004@gmail.com>
2024-08-20 19:23:29 +0100

    array

--- word.js
+++ word.js
@@ -11,3 +11,14 @@
 // window.alert('heyy there')
 
 // tgis is t\a rest wdkrh/
+
+function checkscore(){
+    let inputScore = document.getElementById('inputScore').value
+
+if (inputScore=> 80 && inputScore <=100) {
+    alert(`${inputScore} grade is A`)
+    }else if (inputScore =>70 && inputScore <=79) {
+        alert(`${inputScore} grade is B`)
+    }
+
+}
